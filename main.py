b28cf1b79514114306d87def5f095ba56c718575
Keagan Stokoe <45765811+KeaganStokoe@users.noreply.github.com>
2022-06-01 04:58:32 +0000

    file clean up

--- main.py
+++ main.py
@@ -13,12 +13,8 @@ user_id = '16643d73-456e-4f5a-af85-b6639bfa9eed'
 headers = {'Accept': 'application/json', 'x-vital-api-key': vital_api_key}
 params = {'start_date': start_date, 'end_date': end_date}
 
-
 api_url = f'https://api.sandbox.tryvital.io/v2/summary/workouts/{user_id}'
 
 r = requests.get(api_url, headers=headers, params=params)
-
 res = json.dumps(r.json(), indent=2)
-
-
 print(res)
