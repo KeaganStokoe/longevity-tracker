a2f98951454cf9eb989c70f8b4c788e905a0ac28
Keagan Stokoe <45765811+KeaganStokoe@users.noreply.github.com>
2022-06-02 04:59:25 +0000

    create function to retrieve running workout data

--- main.py
+++ main.py
@@ -13,8 +13,24 @@ user_id = '16643d73-456e-4f5a-af85-b6639bfa9eed'
 headers = {'Accept': 'application/json', 'x-vital-api-key': vital_api_key}
 params = {'start_date': start_date, 'end_date': end_date}
 
-api_url = f'https://api.sandbox.tryvital.io/v2/summary/workouts/{user_id}'
+def getWorkoutData():
+  """
+  Function to request all workout data
+  """
+  api_url: str = f'https://api.sandbox.tryvital.io/v2/summary/workouts/{user_id}'
+  r = requests.get(api_url, headers=headers, params=params)
+  # res = json.dumps(r.json(), indent=2)
+  return r
 
-r = requests.get(api_url, headers=headers, params=params)
-res = json.dumps(r.json(), indent=2)
-print(res)
+def getRunningData():
+  """
+  Function that returns 
+  """
+  res = getWorkoutData()
+  for item in res.json()["workouts"]:
+    if item['sport']['name'] == "Running":
+      print("yes")
+
+getRunningData()
+    
+    
